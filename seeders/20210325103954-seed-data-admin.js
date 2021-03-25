260b4d3364c1bc85e2557288a1d15ac2265b6d0c
billybep <billy.pesoth@gmail.com>
2021-03-26 03:34:31 +0700

    add seeding - minor fix bug

--- seeders/20210325103954-seed-data-admin.js
+++ seeders/20210325103954-seed-data-admin.js
@@ -1,17 +1,44 @@
 'use strict';
 
+const bcrypt = require('bcryptjs')
+const salt = bcrypt.genSaltSync(7)
+
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Users', [{
-      name: 'admin',
-      username: 'superuser',
-      password: 'admin123',
-      email: 'admin@mail.com',
-      gender: 'male',
-      isAdmin: true,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }])
+    return queryInterface.bulkInsert('Users', 
+    [
+      {
+        name: 'admin',
+        username: 'superuser',
+        password: bcrypt.hashSync('123',salt),
+        email: 'admin@mail.com',
+        gender: 'male',
+        isAdmin: true,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'nia',
+        username: 'nia',
+        password: bcrypt.hashSync('123',salt),
+        email: 'nia@mail.com',
+        gender: 'female',
+        isAdmin: true,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'users',
+        username: 'users',
+        password: bcrypt.hashSync('123',salt),
+        email: 'user@mail.com',
+        gender: 'male',
+        isAdmin: false,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ])
   },
 
   down: (queryInterface, Sequelize) => {
